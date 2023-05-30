d533f3ebb0e527827500f6cb1e49feca0e87c027
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-05-30 08:10:18 +0300

    refactor: delete console.log

--- frontend/src/contexts/AuthProvider.jsx
+++ frontend/src/contexts/AuthProvider.jsx
@@ -2,13 +2,9 @@ import { useMemo, useState } from 'react';
 import AuthContext from './index';
 
 const AuthProvider = ({ children }) => {
-  // console.dir(localStorage);
   const currentUser = JSON.parse(localStorage.getItem('user'));
-  // console.log(currentUser);
   const userName = currentUser ? { username: currentUser.username } : null;
-  // console.log(userName);
   const [user, setUser] = useState(userName);
-  // console.log('user', user);
 
   const memoContext = useMemo(() => {
     const logIn = (userData) => {
