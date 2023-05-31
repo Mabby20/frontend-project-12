d6fdc39ede35646c8aaa196bad0b3456dd6d5815
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-05-31 19:57:12 +0300

    fix: replace content text to use i18n

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,16 +1,17 @@
 import { Container, Navbar } from 'react-bootstrap';
 import { Link, Outlet } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import AuthButton from './AuthButton';
 import { appPaths } from '../../routes';
 
 const NavBar = () => {
-  const fist = 'f';
+  const { t } = useTranslation();
   return (
     <>
       <Navbar bg="white" expand="lg" className="shadow-sm">
         <Container>
           <Navbar.Brand as={Link} to={appPaths.chatPagePath}>
-            Hexlet Chat
+            {t('LogoName')}
           </Navbar.Brand>
           <AuthButton />
         </Container>
