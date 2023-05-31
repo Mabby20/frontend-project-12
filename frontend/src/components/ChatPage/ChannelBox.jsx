849251cfb3fd114fcaaac095b82a9ffe741b02a3
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-05-31 19:51:50 +0300

    fix: replace content text to use i18n

--- frontend/src/components/ChatPage/ChannelBox.jsx
+++ frontend/src/components/ChatPage/ChannelBox.jsx
@@ -1,17 +1,18 @@
 import { Col, Nav } from 'react-bootstrap';
 import { PatchPlus } from 'react-bootstrap-icons';
 import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { selectors as channelsSelectors } from '../../slices/channelsSlice';
 import { actions as modalsActions } from '../../slices/modalSlice';
 import Channel from './Channel';
 
 const ChannelBox = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const channels = useSelector(channelsSelectors.selectAll);
   const currentChannelId = useSelector(
     (state) => state.channelsInfo.currentChannelId,
   );
-  console.log(channels);
 
   const handleAddChannelClick = () => {
     dispatch(
@@ -25,7 +26,7 @@ const ChannelBox = () => {
   return (
     <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-        <b>Каналы</b>
+        <b>{t('channels')}</b>
         <button
           onClick={handleAddChannelClick}
           className="p-0 text-primary btn btn-group-vertical"
