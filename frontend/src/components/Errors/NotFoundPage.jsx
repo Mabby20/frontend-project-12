d281f021040f98e6c996a3a90f4f287a5b235908
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-05-31 19:57:24 +0300

    fix: replace content text to use i18n

--- frontend/src/components/Errors/NotFoundPage.jsx
+++ frontend/src/components/Errors/NotFoundPage.jsx
@@ -1,18 +1,22 @@
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import { useTranslation } from 'react-i18next';
 import { appPaths } from '../../routes';
 import errorImg from '../../assets/error.svg';
 
-const NotFoundPage = () => (
-  <div className="text-center">
-    <Image className="h-25" alt="Страница не найдена" src={errorImg} fluid />
-    <h1 className="h4 text-muted">Страница не найдена</h1>
-    <p className="text-muted">
-      Но вы можете перейти{' '}
-      <Link to={appPaths.chatPagePath}>на главную страницу</Link>
-    </p>
-  </div>
-);
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center">
+      <Image className="h-25" alt="Страница не найдена" src={errorImg} fluid />
+      <h1 className="h4 text-muted">{t('notFoundPage')}</h1>
+      <p className="text-muted">
+        {t('canGo')}{' '}
+        <Link to={appPaths.chatPagePath}>{t('backToMainPage')}</Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFoundPage;
