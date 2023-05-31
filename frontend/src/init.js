64e35bda261ae080ec4764fb8602f77fc3ff0515
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-05-31 19:46:07 +0300

    build: add i18n in project

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,19 +1,38 @@
 import { Provider } from 'react-redux';
 import { io } from 'socket.io-client';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 import AuthProvider from './contexts/AuthProvider';
 import App from './components/App';
 import store from './slices';
 import SocketProvider from './contexts/SocketProvider';
+import resources from './locales/index.js';
 
-const websocket = io();
-const init = () => (
-  <Provider store={store}>
-    <SocketProvider socket={websocket}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </SocketProvider>
-  </Provider>
-);
+const init = async () => {
+  const websocket = io();
+  const i18n = i18next.createInstance();
+
+  await i18n.use(initReactI18next).init({
+    resources,
+    lng: 'ru',
+    fallbackLng: 'ru',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+  return (
+    <Provider store={store}>
+      <SocketProvider socket={websocket}>
+        <AuthProvider>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </AuthProvider>
+      </SocketProvider>
+    </Provider>
+  );
+};
 
 export default init;
