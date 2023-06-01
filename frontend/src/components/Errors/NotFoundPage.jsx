882f495f96c6842f2a4035d47997c9b4d0b1c309
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-01 03:28:02 +0300

    style: change img for component

--- frontend/src/components/Errors/NotFoundPage.jsx
+++ frontend/src/components/Errors/NotFoundPage.jsx
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 
 import { useTranslation } from 'react-i18next';
 import { appPaths } from '../../routes';
-import errorImg from '../../assets/error.svg';
+import error from '../../assets/404error.png';
 
 const NotFoundPage = () => {
   const { t } = useTranslation();
   return (
     <div className="text-center">
-      <Image className="h-25" alt="Страница не найдена" src={errorImg} fluid />
+      <Image
+        width="auto"
+        height="auto"
+        alt="Страница не найдена"
+        src={error}
+        fluid
+      />
       <h1 className="h4 text-muted">{t('notFoundPage')}</h1>
       <p className="text-muted">
         {t('canGo')}{' '}
