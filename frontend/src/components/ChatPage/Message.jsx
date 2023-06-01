8bae3849de174ccb5b9a0cfb1af738e6f256f04c
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-01 04:26:47 +0300

    feat: add messages text censorship

--- frontend/src/components/ChatPage/Message.jsx
+++ frontend/src/components/ChatPage/Message.jsx
@@ -1,9 +1,14 @@
-const Message = ({ message }) => (
-  <div className="text-break mb-2">
-    <b>{message.user}</b>
-    {': '}
-    {message.body}
-  </div>
-);
+import filter from 'leo-profanity';
+
+const Message = ({ message }) => {
+  const messageText = filter.clean(message.body);
+  return (
+    <div className="text-break mb-2">
+      <b>{message.user}</b>
+      {': '}
+      {messageText}
+    </div>
+  );
+};
 
 export default Message;
