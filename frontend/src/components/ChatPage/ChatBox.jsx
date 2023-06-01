257b628c1325381715e9d0f1471d6289cb24a15e
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-01 04:24:17 +0300

    feat: add channel name censorship

--- frontend/src/components/ChatPage/ChatBox.jsx
+++ frontend/src/components/ChatPage/ChatBox.jsx
@@ -1,6 +1,7 @@
 import { Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import filter from 'leo-profanity';
 import FormMessage from './FormMessage';
 import Message from './Message';
 import { selectors as messagesSelectors } from '../../slices/messageSlice';
@@ -9,6 +10,7 @@ import { selectors as channelsSelectors } from '../../slices/channelsSlice';
 const ChatBox = () => {
   const { t } = useTranslation();
   const currentChannel = useSelector(channelsSelectors.selectCurrentChannel);
+  const channelName = filter.clean(currentChannel.name);
   const messagesForCurrentChannel = useSelector(
     messagesSelectors.selectMessagesById,
   );
@@ -17,7 +19,7 @@ const ChatBox = () => {
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0">
-            <b># {currentChannel.name}</b>
+            <b># {channelName}</b>
           </p>
           <span className="text-muted">
             {t('message', { count: messagesForCurrentChannel.length })}
