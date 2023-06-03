4f6a48f70903e7dc5ec27a589fb8bfb0f7785206
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-04 01:24:02 +0300

    feat: init filterContext

--- frontend/src/contexts/index.jsx
+++ frontend/src/contexts/index.jsx
@@ -2,5 +2,6 @@ import { createContext } from 'react';
 
 const AuthContext = createContext({});
 export const SocketContext = createContext({});
+export const FilterContext = createContext({});
 
 export default AuthContext;
