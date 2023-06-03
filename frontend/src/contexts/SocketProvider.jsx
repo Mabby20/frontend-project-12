92d5ff9e63542265fc7622b97f97bf00e23f471a
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-04 01:18:15 +0300

    feat: use useCallback in provider

--- frontend/src/contexts/SocketProvider.jsx
+++ frontend/src/contexts/SocketProvider.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { actions as messagesActions } from '../slices/messageSlice';
 import { actions as channelsActions } from '../slices/channelsSlice';
@@ -7,66 +7,88 @@ import { SocketContext } from './index';
 const SocketProvider = ({ socket, children }) => {
   const dispatch = useDispatch();
 
-  const memoContext = useMemo(() => {
-    const connectSocket = () => {
-      socket.connect();
+  const connectSocket = useCallback(() => {
+    socket.connect();
 
-      socket.on('newMessage', (message) => {
-        console.log('new message', message);
-        dispatch(messagesActions.addMessage(message));
-      });
+    socket.on('newMessage', (message) => {
+      console.log('new message', message);
+      dispatch(messagesActions.addMessage(message));
+    });
 
-      socket.on('newChannel', (channel) => {
-        console.log('new channel', channel);
-        dispatch(channelsActions.addChanel(channel));
-        dispatch(channelsActions.changeCurrentChannelId({ id: channel.id }));
-      });
+    socket.on('newChannel', (channel) => {
+      console.log('new channel', channel);
+      dispatch(channelsActions.addChanel(channel));
+      dispatch(channelsActions.changeCurrentChannelId({ id: channel.id }));
+    });
 
-      socket.on('removeChannel', ({ id }) => {
-        console.log('id by remove channel', id);
-        dispatch(channelsActions.removeChannel(id));
-      });
+    socket.on('removeChannel', ({ id }) => {
+      console.log('id by remove channel', id);
+      dispatch(channelsActions.removeChannel(id));
+    });
 
-      socket.on('renameChannel', (channel) => {
-        console.log('rename channel', channel);
-        dispatch(
-          channelsActions.renameChannel({
-            id: channel.id,
-            changes: { name: channel.name },
-          }),
-        );
-      });
-    };
+    socket.on('renameChannel', (channel) => {
+      console.log('rename channel', channel);
+      dispatch(
+        channelsActions.renameChannel({
+          id: channel.id,
+          changes: { name: channel.name },
+        }),
+      );
+    });
+  }, [dispatch, socket]);
 
-    const disconnectSocket = () => {
-      socket.disconnect();
-    };
+  const disconnectSocket = useCallback(() => {
+    socket.disconnect();
+  }, [socket]);
 
-    const sendMessage = async (message) => {
+  const sendMessage = useCallback(
+    async (message) => {
       await socket.timeout(3000).emitWithAck('newMessage', message);
-    };
-    const addNewChannel = async (channel) => {
-      await socket.timeout(3000).emitWithAck('newChannel', channel);
-    };
+    },
+    [socket],
+  );
 
-    const removeChannel = async (targetId) => {
+  const addNewChannel = useCallback(
+    async (channel) => {
+      await socket.timeout(3000).emitWithAck('newChannel', channel);
+    },
+    [socket],
+  );
+
+  const removeChannel = useCallback(
+    async (targetId) => {
       await socket.timeout(3000).emitWithAck('removeChannel', { id: targetId });
-    };
-    const renameChannel = async (updateChannelInfo) => {
+    },
+    [socket],
+  );
+
+  const renameChannel = useCallback(
+    async (updateChannelInfo) => {
       await socket
         .timeout(3000)
         .emitWithAck('renameChannel', updateChannelInfo);
-    };
+    },
+    [socket],
+  );
 
-    return {
+  const memoContext = useMemo(
+    () => ({
       connectSocket,
       sendMessage,
       addNewChannel,
       removeChannel,
       renameChannel,
       disconnectSocket,
-    };
-  }, [dispatch, socket]);
+    }),
+    [
+      addNewChannel,
+      connectSocket,
+      disconnectSocket,
+      removeChannel,
+      renameChannel,
+      sendMessage,
+    ],
+  );
 
   return (
     <SocketContext.Provider value={memoContext}>
