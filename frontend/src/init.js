657dd63ac94810b44364da469b1c3410e7883bee
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-04 01:24:35 +0300

    feat: use FilterProvider

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,9 +2,9 @@ import { Provider } from 'react-redux';
 import { io } from 'socket.io-client';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import filter from 'leo-profanity';
 import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
 import AuthProvider from './contexts/AuthProvider';
+import FilterProvider from './contexts/FilterProvider';
 import App from './components/App';
 import store from './slices';
 import SocketProvider from './contexts/SocketProvider';
@@ -13,8 +13,6 @@ import resources from './locales/index.js';
 const init = async () => {
   const websocket = io();
   const i18n = i18next.createInstance();
-  const badWordsRu = filter.getDictionary('ru');
-  filter.add(badWordsRu);
 
   const rollbarConfig = {
     accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
@@ -39,9 +37,11 @@ const init = async () => {
         <Provider store={store}>
           <SocketProvider socket={websocket}>
             <AuthProvider>
-              <I18nextProvider i18n={i18n}>
-                <App />
-              </I18nextProvider>
+              <FilterProvider>
+                <I18nextProvider i18n={i18n}>
+                  <App />
+                </I18nextProvider>
+              </FilterProvider>
             </AuthProvider>
           </SocketProvider>
         </Provider>
