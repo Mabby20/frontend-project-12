f63285ab44b68cc49ceef2b7e218daf1a1ef6115
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-06-08 03:58:02 +0300

    fix: add FilterContext

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -3,20 +3,26 @@ import { io } from 'socket.io-client';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
+import filter from 'leo-profanity';
 import AuthProvider from './contexts/AuthProvider';
-import FilterProvider from './contexts/FilterProvider';
 import App from './components/App';
 import store from './slices';
 import SocketProvider from './contexts/SocketProvider';
 import resources from './locales/index.js';
+import { FilterContext } from './contexts/index';
 
 const init = async () => {
+  const badWordsRu = filter.getDictionary('ru');
+  filter.add(badWordsRu);
+
+  const filterBadWord = (word) => filter.clean(word);
+
   const websocket = io();
   const i18n = i18next.createInstance();
 
   const rollbarConfig = {
     accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
-    environment: 'production',
+    environment: process.env.NODE_ENV,
     captureUncaught: true,
     captureUnhandledRejections: true,
   };
@@ -37,11 +43,11 @@ const init = async () => {
         <Provider store={store}>
           <SocketProvider socket={websocket}>
             <AuthProvider>
-              <FilterProvider>
+              <FilterContext.Provider value={filterBadWord}>
                 <I18nextProvider i18n={i18n}>
                   <App />
                 </I18nextProvider>
-              </FilterProvider>
+              </FilterContext.Provider>
             </AuthProvider>
           </SocketProvider>
         </Provider>
